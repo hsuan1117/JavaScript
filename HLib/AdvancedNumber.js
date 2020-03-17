8e84bd2e66a6504340224de9c8bf0b5719eefec8
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-17 22:47:37 +0800

    Update AdvancedNumber.js

--- HLib/AdvancedNumber.js
+++ HLib/AdvancedNumber.js
@@ -3,7 +3,7 @@ class AdvanceNumber extends HLib {
     
   }
   get average() {
-    
+    [0, 1, 2, 3, 4].reduce((p, curr) => prev + curr )
   }
   constructor(data = [] , settings) {
     //存放計算的數組
