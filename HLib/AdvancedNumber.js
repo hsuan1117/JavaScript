719f51299510b5e4d82f1fbb97d3cba0b955d331
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-17 23:59:22 +0800

    Update AdvancedNumber.js

--- HLib/AdvancedNumber.js
+++ HLib/AdvancedNumber.js
@@ -1,11 +1,11 @@
 class AdvanceNumber extends HLib {
   get sigma() {
-    let _=0;
-    for(let i=0;i<this.data.length;i++)_+=this.data[i]*this.data[i]
+    let _=this.average;
+    this.data.reduce((p,c)=>p+=(c-_)*(c-_))
     
   }
   get average() {
-    return this.data.reduce((p,c)=>p+c)/this.data.length
+    return this.data.reduce((p,c)=>p+=c)/this.data.length
   }
   constructor(data = [] , settings) {
     //存放計算的數組
