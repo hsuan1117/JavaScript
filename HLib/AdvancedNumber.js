98c359bc58512317482c84f627d00a59993a7e09
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-18 00:32:51 +0800

    Update AdvancedNumber.js

--- HLib/AdvancedNumber.js
+++ HLib/AdvancedNumber.js
@@ -10,9 +10,14 @@ class AdvanceNumber extends HLib {
     return this.data.reduce((p,c)=>p+=c,0)/this.data.length
   }
   get me() {
-    let x = this.data
+    let x = this.data,r=0
     x.sort(Algorithm.less)
-    
+    if(x.length%2 == 0){
+      r = (x[(x.length-1)/2] + x[x.length/2] ) / 2.0;
+    }else{
+      r = x[Math.floor(x.length/2)]
+    }
+    return r;
   }
   constructor(data = [] , settings) {
     super()
