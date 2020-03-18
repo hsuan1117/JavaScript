00b00042e6f7da96ce32d14df409c4151baf667f
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-18 20:58:48 +0800

    Update DOM.js

--- HLib/DOM.js
+++ HLib/DOM.js
@@ -1,7 +1,6 @@
 class DOM extends HLib {
   constructor(selector){
     this.selector = selector
-    if(selector[0]=="#")
-      this.elem = document.getElementById(selector.substr(1))
+    this.elems    = document.querySelectorAll(selector)
   }
 }
