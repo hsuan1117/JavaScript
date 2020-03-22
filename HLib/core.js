849d10c66e07990df36ec3921aa0c9be9a6e31d3
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-22 21:54:29 +0800

    Update core.js

--- HLib/core.js
+++ HLib/core.js
@@ -1,3 +1,19 @@
 class HLib {
-  
+  constructor(includesPath){
+    this.path = includesPath
+  }
+  startWith(str,x){
+    if(str[0] == x) {
+      return 1;
+    }else{
+      return 0;
+    }
+  }
+  endWith(str,x){
+    if(str[str.length-1] == x) {
+      return 1;
+    }else{
+      return 0;
+    }
+  }
 }
